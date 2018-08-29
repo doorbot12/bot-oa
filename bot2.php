f38c84430dbad15d08c915c897d4275eb54e7844
doorbot12 <barubeno@gmail.com>
2018-08-29 18:52:44 +0000

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -263,7 +263,7 @@ function lirik($keyword) {
 }
 #-------------------------[Function]-------------------------#
 function music($keyword) { 
-    $uri = "https://rest.farzain.com/api/joox.php?id=" . $keyword . ""; 
+    $uri = "https://rest.farzain.com/api/joox.php?songname=" . $keyword . ""; 
  
     $response = Unirest\Request::get("$uri"); 
  
